ac9804764c552b8deb8e12e9bb018f6b9efffd62
yuliamrg <yuliam.rivera@outlook.com>
2022-08-15 00:44:44 -0500

    agego funcion de espar y calcualr mediana de un array

--- promedio.js
+++ promedio.js
@@ -1,14 +1,31 @@
 const arr = [1, 4, 7, 2, 4];
+const arr2 = [1, 2, 3, 4,];
+const once = [1,2,3,4,5,6,7,8,9,10,11];
+
+//Elementos del HTML
+
 const lengthButton = document.querySelector('#butttonConfirm');
 const clearButton = document.querySelector('#buttonClear');
 const formNumbers = document.querySelector('#promNumbers');
 const lengthNumbers = document.querySelector('#lenght');
 
-const promWarn = document.createElement('p'); 
+
+// Elementos creados
+
+const promWarn = document.createElement('span'); 
 promWarn.classList.add('warn');
+const promButton = document.createElement('button');
+    promButton.innerText = 'Promedio';
+
+
+//addeventslisteners
 
 lengthButton.addEventListener('click', createInputs);
 clearButton.addEventListener('click', clearInputs);
+promButton.addEventListener('click', calculateProm);
+
+
+//funciones
 
 function clearInputs() {
     const large = formNumbers.length;
@@ -30,13 +47,11 @@ function createInputs() {
     clearInputs();
 
     if (numbers > 1 && numbers < 11) {
-        const promButton = document.createElement('button');
-    promButton.setAttribute('type', 'button');
-    promButton.innerText = 'Calcular';
 
         for (let i=0; i < numbers; i++ ) { 
             const input = document.createElement('input');
             input.classList.add('inputNumbers');
+            input.setAttribute('required','');
             formNumbers.appendChild(input);
         }
         formNumbers.appendChild(promButton);
@@ -46,8 +61,10 @@ function createInputs() {
     }
     formNumbers.classList.remove('inactive');
 }
-    
-const promedio = calcularPromedio(arr);
+
+function calculateProm() {
+
+}
 
 function calcularPromedio(array) {
     let acumulado = 0;
@@ -58,4 +75,34 @@ function calcularPromedio(array) {
     }
 
     return acumulado / longitud;
+}
+
+function calcularPromedioReduce(array) {
+    const acumulado = array.reduce((valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor);
+    return acumulado / array.length;
+}
+
+function esPar(array) {
+    const long = array.length;
+    if (long % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calcularMediana(array) {
+    const arrayPAr = esPar(array);
+    const long = array.length;
+    let mediana;
+    if (arrayPAr) {
+        const mitad1 = array[long / 2];
+        const mitad2 = array[(long / 2) - 1];
+        mediana = calcularPromedioReduce([mitad1, mitad2]);
+        return mediana;
+    } else {
+        mediana = long / 2;
+        console.log(mediana);
+        return array[mediana];
+    }
 }
